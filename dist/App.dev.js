dcb58dd71e63be73089d46d99a0b5147c356ba94
AlladaMohan21 <mohanallada7@gmail.com>
2025-04-25 19:20:38 +0530

    Episode-01

---
+++ dist/App.dev.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var parent = [React.createElement("div", {
+  id: "parent"
+}, React.createElement("div", {
+  id: "child"
+}, [React.createElement("h1", {}, "I am a h1 tag"), React.createElement("h2", {}, "I am a h2 tag")])), React.createElement("div", {
+  id: "parent"
+}, React.createElement("div", {
+  id: "child2"
+}, [React.createElement("h1", {}, "I am a h1 tag"), React.createElement("h2", {}, "I am a h2 tag")]))];
+console.log(parent);
+var root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
+//# sourceMappingURL=App.dev.js.map
