9b5b47cc81457216a9e69e48c165c70e8dbebb7d
AlladaMohan21 <mohanallada7@gmail.com>
2025-04-26 17:47:25 +0530

    new commit

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-const parent= [React.createElement("div",{id:"parent"},
-    React.createElement("div",{id:"child"},
-        [React.createElement("h1",{},"I am a h1 tag"),React.createElement("h2",{},"I am a h2 tag")])),
-        React.createElement("div",{id:"parent"},
-            React.createElement("div",{id:"child2"},
-                [React.createElement("h1",{},"I am a h1 tag"),React.createElement("h2",{},"I am a h2 tag")]))];
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-        console.log(parent);
+const parent = [
+  React.createElement("div", { key: "1", id: "parent1" },
+    React.createElement("div", { id: "child1" }, [
+      React.createElement("h1", {}, "I am a h1 tag"),
+      React.createElement("h2", {}, "I am a h2 tag")
+    ])
+  ),
+  React.createElement("div", { key: "2", id: "parent2" },
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I am a h1 tag"),
+      React.createElement("h2", {}, "I am a h2 tag")
+    ])
+  )
+];
 
-const root=ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
