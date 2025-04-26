3e8e503f41812dda28ceaa77706d9fe7133b2ccd
AlladaMohan21 <mohanallada7@gmail.com>
2025-04-26 19:29:10 +0530

    commited

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = [
-  React.createElement("div", { key: "1", id: "parent1" },
-    React.createElement("div", { id: "child1" }, [
-      React.createElement("h1", {}, "I am a h1 tag"),
-      React.createElement("h2", {}, "I am a h2 tag")
-    ])
-  ),
-  React.createElement("div", { key: "2", id: "parent2" },
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I am a h1 tag"),
-      React.createElement("h2", {}, "I am a h2 tag")
-    ])
-  )
-];
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+const Head=()=>(<h1 id="heading" className="header">This is a code by jsx in java script </h1>);
+const HeaderComponent=()=>(
+<div id="container">
+    <Head />
+    <h1>Header by component composition</h1>
+ </div>
+);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent />);
