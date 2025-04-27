6c53e6ddc4a9fc6a68b6e9dfba66959c9ecff7da
AlladaMohan21 <mohanallada7@gmail.com>
2025-04-27 17:39:05 +0530

    commited

--- dist/App.dev.js
+++ dist/App.dev.js
@@ -1,15 +1,24 @@
 "use strict";
 
-var parent = [React.createElement("div", {
-  id: "parent"
-}, React.createElement("div", {
-  id: "child"
-}, [React.createElement("h1", {}, "I am a h1 tag"), React.createElement("h2", {}, "I am a h2 tag")])), React.createElement("div", {
-  id: "parent"
-}, React.createElement("div", {
+var _react = _interopRequireDefault(require("react"));
+
+var _client = _interopRequireDefault(require("react-dom/client"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var parent = [_react["default"].createElement("div", {
+  key: "1",
+  id: "parent1"
+}, _react["default"].createElement("div", {
+  id: "child1"
+}, [_react["default"].createElement("h1", {}, "I am a h1 tag"), _react["default"].createElement("h2", {}, "I am a h2 tag")])), _react["default"].createElement("div", {
+  key: "2",
+  id: "parent2"
+}, _react["default"].createElement("div", {
   id: "child2"
-}, [React.createElement("h1", {}, "I am a h1 tag"), React.createElement("h2", {}, "I am a h2 tag")]))];
-console.log(parent);
-var root = ReactDOM.createRoot(document.getElementById("root"));
+}, [_react["default"].createElement("h1", {}, "I am a h1 tag"), _react["default"].createElement("h2", {}, "I am a h2 tag")]))];
+
+var root = _client["default"].createRoot(document.getElementById("root"));
+
 root.render(parent);
 //# sourceMappingURL=App.dev.js.map
